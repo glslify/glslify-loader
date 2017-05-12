b7db26d403b8e3d561c4f1b14081f2dcee45c6f3
pqml <hugo.piquemal@gmail.com>
2017-05-12 04:02:07 +0200

    Update code to work with glslify-deps and glslify-bundle

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-const glslify = require('glslify')
-const path    = require('path')
+// use 'use strict' to avoid let/const errors with old node versions
+'use strict'
 
-module.exports = glslifyWebpackLoader
+const path = require('path')
+const loaderUtils = require('loader-utils')
+const deps = require('glslify-deps')
+const bundle = require('glslify-bundle')
 
-function glslifyWebpackLoader(source) {
-  var basedir = path.dirname(this.resourcePath)
-  var self = this
+function glslifyLoader (content) {
+  this.cacheable && this.cacheable()
+  const callback = this.async()
 
-  this.callback = this.async()
-  this.cacheable(true)
+  // Parse querystring from require path
+  let query = this.query || {}
+  if (typeof query === 'string') {
+    if (query[0] !== '?') query = '?' + query
+    query = loaderUtils.parseQuery(query)
+  }
 
-  glslify.bundle(source, {
-    inline: true,
-    basedir: basedir
-  }, function(err, src, files) {
-    if (files) {
-      files.forEach(function(file) {
-        self.addDependency(file)
-      })
-    }
+  const depper = deps()
+  const basedir = path.dirname(this.resourcePath)
 
-    return self.callback(err, src)
+  // Add transforms to the shader and its dependencies
+  const transforms = Array.isArray(query.transform) ? query.transform : []
+  transforms.forEach(transform => {
+    if (typeof transform !== 'string' && !Array.isArray(transform)) return
+    const name = Array.isArray(transform) ? transform[0] : transform
+    const options = Array.isArray(transform) && transform.length > 1
+      ? transform[1] : {}
+    depper.transform(name, options)
+  })
+
+  // Build the dependency graph
+  depper.inline(content, basedir, (err, files) => {
+    if (err) return callback(err, null)
+    files && files.forEach(file => !file.entry && this.addDependency(file.file))
+    const output = bundle(files)
+    return callback(err, output)
   })
 }
+
+module.exports = glslifyLoader
